f3b19e9fcb05e27bfc69bab2e8e9a766731f5dac
Bernardo <be.zito@hotmail.com>
2020-03-19 11:42:51 -0300

    implementando express.js e mudanças drásticas

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000)
+app.use( '/add-product', (req,res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req,res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use( '/', (req,res, next) => {
+    res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000);
