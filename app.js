d9e1e5e1823cee782bb59a09440eaef41534424d
Bernardo <be.zito@hotmail.com>
2020-03-19 16:42:18 -0300

    pasta routes e pasta views

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes  = require('./routes/shop' );
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use( '/add-product', (req,res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.post('/product', (req,res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-app.use( '/', (req,res, next) => {
-    res.send('<h1>Hello from express</h1>');
-});
-
-app.listen(3000);
+app.listen(3000);  
