827f795294766cd817955a648ae1a6723c2f8add
Bernardo <be.zito@hotmail.com>
2020-03-20 04:33:33 -0300

    template pug

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes  = require('./routes/shop' );
@@ -15,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {docTitle: 'Page Not Found'})
 });
 
 app.listen(3000);  
