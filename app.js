45a0b90c71ed7d018fc8080b4f7153c33db212e4
Bernardo <be.zito@hotmail.com>
2020-03-20 17:46:15 -0300

    add ejs and layouts

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not Found'})
+    res.status(404).render('404', {docTitle: 'Page Not Found', path:'404'})
 });
 
 app.listen(3000);  
