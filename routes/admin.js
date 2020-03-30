be5046786c6bf2cfc7d8996ea3fd3a97bcb2def6
Bernardo <be.zito@hotmail.com>
2020-03-30 18:43:27 -0300

    final do módulo sequelize

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,21 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const rootDir = require('../util/path');
-
-
+// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-router.get('/products', adminController.getAdminProducts)
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
-
-router.post('/add-product', adminController.postAddProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
 
