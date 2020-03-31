0fe40d871d2380c1d08c587942fbe4b18c2ff031
Bernardo <be.zito@hotmail.com>
2020-03-30 23:18:04 -0300

    conectando ao mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'mousetrap123', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://bernardo:eLdHZvcimLJgqLdS@cluster0-8qvme.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true })
+.then(client => {
+    console.log("conected");
+    callback(client);
+})
+.catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
