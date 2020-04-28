d9c365217f2ceb91286a3f22f4d12f2a06c8e8d2
Bernardo <be.zito@hotmail.com>
2020-04-28 04:05:45 -0300

    autenticação e password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,6 @@ exports.getSignup = (req, res, next) => {
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
     errorMessage: message,
   });
 };
@@ -161,4 +160,53 @@ exports.postReset = (req,res,next) => {
     })
     .catch(err =>console.log(err))
   });
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExpiration: {$gt: Date.now()}})
+  .then(user =>{
+    let message = req.flash('error');
+    if (message.length > 0) {
+      massage = message[0];
+    } else {
+      message = null;
+    }
+    res.render("auth/new-password", {
+      path: "/new-password",
+      pageTitle: "New Password",
+      errorMessage: message,
+      userId: user._id.toString(),
+      passwordToken: token
+    });
+  })
+  .catch(err => console.log(err))
+
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const token = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: {$gt: Date.now()},
+    _id: userId
+    })
+    .then(user => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12)
+    })
+    .then(hashedPassword =>{
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => console.log(err))
 }
